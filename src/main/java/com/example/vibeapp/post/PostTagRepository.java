4a5ad56b8a230f1874f44008c707c8f2843cfd18
choplay-google <choplay@gmail.com>
2026-02-24 15:05:44 +0900

    feat: PostTag 엔티티, Repository 및 Mapper 추가

---
+++ src/main/java/com/example/vibeapp/post/PostTagRepository.java
@@ -0,0 +1,10 @@
+package com.example.vibeapp.post;
+
+import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
+
+@Mapper
+public interface PostTagRepository {
+    void save(PostTag postTag);
+    void deleteByPostNo(Long postNo);
+}
